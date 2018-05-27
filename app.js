877b04b17e37fd443558852f9d70cc038b59f1aa
sdbowen <sdbowen.git@gmail.com>
2018-05-27 14:58:00 -0500

    Add code formatting

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-env browser */
 const key = config.mapsKey;
 
 document.getElementById('menu-toggle').addEventListener('click', (e) => {
@@ -6,35 +6,35 @@ document.getElementById('menu-toggle').addEventListener('click', (e) => {
   e.preventDefault();
 });
 
-var position = [27.1959739, 78.02423269999997];
+const position = [27.1959739, 78.02423269999997];
 
 function loadScript() {
-  var script = document.createElement('script');
+  const script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?' +
-    'key=' + key + '&callback=showGoogleMaps';
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=showGoogleMaps`;
   document.body.appendChild(script);
 }
 
 document.addEventListener('DOMContentLoaded', loadScript);
 
 function showGoogleMaps() {
+  const latLng = new google.maps.LatLng(position[0], position[1]);
 
-  var latLng = new google.maps.LatLng(position[0], position[1]);
-
-  var mapOptions = {
+  const mapOptions = {
     zoom: 16, // initialize zoom level - the max value is 21
     streetViewControl: false, // hide the yellow Street View pegman
     scaleControl: true, // allow users to zoom the Google Map
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: latLng
+    center: latLng,
   };
 
-  map = new google.maps.Map(document.getElementById('googleMap'),
-    mapOptions);
+  const map = new google.maps.Map(
+    document.getElementById('googleMap'),
+    mapOptions,
+  );
 
   // Show the default red marker at the location
-  marker = new google.maps.Marker({
+  const marker = new google.maps.Marker({
     position: latLng,
     map: map,
     draggable: false,
