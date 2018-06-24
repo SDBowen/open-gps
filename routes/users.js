30e34168512a0800c87ec56c4d10aaa9a1677217
sdbowen <sdbowen.git@gmail.com>
2018-06-24 09:42:44 -0500

    Add user register validation

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 
 const router = express.Router();
 
+// Load input validation
+const validateRegisterInput = require('../validation/register');
+
 // Load user model
 const User = require('../models/User');
 
@@ -18,6 +21,12 @@ router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
 // @Desc    Register user
 // @Access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: 'Email already registered' });
---
+++ validation/isEmpty.js
@@ -0,0 +1,9 @@
+// Validator isEmpty will only validate empty strings. Below function will be used to check for both empty Objects and Strings.
+
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).length === 0) ||
+  (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
