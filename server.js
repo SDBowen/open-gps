3493b87b25103a9998539067231eb3537ce30ccb
sdbowen <sdbowen.git@gmail.com>
2018-06-23 22:55:31 -0500

    Add router files

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// Routes
+const users = require('./routes/api/users');
+const devices = require('./routes/api/devices');
+const reports = require('./routes/api/reports');
 
 const app = express();
 
+// DB Config
+const db = require('./config/config').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('hello'));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/devices', devices);
+app.use('/api/reports', reports);
 
 const port = process.env.PORT || 5000;
 
